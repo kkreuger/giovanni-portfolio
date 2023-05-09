57c44ce8b4fc16df505a471ea4c2744ba60d2cd4
kkreuger <kreuger.k@gmail.com>
2023-05-09 15:48:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,118 @@
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+// Animation - Page transition In
+function pageTransition() {
+    const tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 1.2,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+    const tl = gsap.timeline()
+    tl.from("main", { duration: 1, x: -500, opacity: 0, stagger: 0.4, delay: 0.2 })
+}
+
+$(function () {
+    barba.init({
+        sync: true,
+        transitions: [
+            {
+                async leave(data) {
+                    const done = this.async()
+
+                    pageTransition()
+                    await delay(1000)
+                    done()
+                },
+
+                async enter(data) {
+                    if(data.next.namespace === 'home') playSlider()
+                    contentAnimation()
+                },
+
+                async once(data) {
+                    if(data.next.namespace === 'home') playSlider()
+                    contentAnimation()
+                },
+            },
+        ],
+    });
+});
+
+barba.hooks.before(() => {
+    scroll.stop();
+});
+
+barba.hooks.enter(() => {
+    scroll.scrollTo({
+        offset: 0,
+        smooth: false,
+        disableLerp: true,
+        duration: 0
+    });
+
+    scroll.update();
+});
+
+barba.hooks.after(() => {
+    scroll.update();
+    scroll.start();
+});
+
+function toggleMenu() {
+    document.querySelector('.slide-nav').classList.toggle('open')
+}
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    mobile: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
+});
+
+window.addEventListener("load", (event) => {
+    scroll.update()
+});
+
+window.onresize = scroll.update();
+
+//IMAGE CAROUSEL
+function playSlider() {
+    $('.client-slider').slick({
+        autoplay: true,
+        slidesToShow: 2,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+};
